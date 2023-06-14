0f5d6eb972c0e1c8fd32d14740d531f0dd9b588f
willco213 <87068102+willco123@users.noreply.github.com>
2023-06-14 09:34:39 +0100

    readme

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,8 +2,8 @@ import mongoose, { ConnectOptions } from "mongoose";
 
 let mongoDB =
   process.env.NODE_ENV === "production"
-    ? `mongodb+srv://${process.env.ATLAS_USER}:${process.env.ATLAS_PASSWORD}@${process.env.ATLAS_CLUSTER}}/?retryWrites=true&w=majority`
-    : `mongodb://127.0.0.1/${process.env.LOCAL_DB_NAME}`;
+    ? `mongodb+srv://${process.env.ATLAS_USER}:${process.env.ATLAS_PASSWORD}@willcocluster.imgveza.mongodb.net/?retryWrites=true&w=majority`
+    : "mongodb://127.0.0.1/dogDB";
 
 mongoose
   .connect(mongoDB, {
